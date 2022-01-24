a7c43fc93fdaba267f5a8ad34a204ab492dcf8ce
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-24 12:23:05 +0100

    Use string constant

--- webui.go
+++ webui.go
@@ -49,8 +49,9 @@ const (
 
 // URL and form parameters
 const (
-	linkParameter   = "link"
-	reasonParameter = "reason"
+	linkParameter    = "link"
+	reasonParameter  = "reason"
+	clusterParameter = "cluster"
 )
 
 // REST API endpoints
@@ -483,7 +484,7 @@ func storeConfiguration(writer http.ResponseWriter, request *http.Request) {
 	form := request.Form
 
 	username := form.Get("username")
-	cluster := form.Get("cluster")
+	cluster := form.Get(clusterParameter)
 	reason := form.Get(reasonParameter)
 	description := form.Get("description")
 	configuration := form.Get("configuration")
